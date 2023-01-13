3787dd0fa3c01164a1970140b7ae93f1744870f9
unknown <tanguy.bastin@gmail.com>
2023-01-13 13:09:08 -0500

    updated the filter with primary genre

--- src/statistic/filters.py
+++ src/statistic/filters.py
@@ -1,18 +1,32 @@
 import django_filters
+from django_filters import CharFilter, ModelChoiceFilter, NumberFilter
+
 from library.models import Album, Band
 
 
 class AlbumFilter(django_filters.FilterSet):
+    title = CharFilter(lookup_expr='icontains', label='Titre contenant:')
+    groupe__name = CharFilter(lookup_expr='icontains', label='Nom du groupe contenant:')
+    date_released = CharFilter(lookup_expr='year', label='Année de sortie:')
+    date_listened = CharFilter(lookup_expr='year', label='Année d\'écoute:')
+    genre_primary = CharFilter(method='filter_tags', label='Genre principal contenant:')
+    rating_lte = NumberFilter(field_name='rating', lookup_expr='lte', label='Note inférieure ou égale à:')
+    rating_gte = NumberFilter(field_name='rating', lookup_expr='gte', label='Note supérieure ou égale à:')
+
     class Meta:
         model = Album
-        fields = {
-            'title': ['icontains'],
-            'groupe__name': ['icontains'],
-            'date_released': ['year'],
-            'date_listened': ['year'],
-            'rating': ['lte', 'gte'],
-            'type_vocal': ['exact'],
-        }
+        fields = [
+            'title',
+            'groupe__name',
+            'date_released',
+            'date_listened',
+            'genre_primary',
+            'rating_lte',
+            'rating_gte',
+        ]
+
+    def filter_tags(self, queryset, name, value):
+        return queryset.filter(genre_primary__name__icontains=value)
 
 
 class BandFilter(django_filters.FilterSet):
